8e3b64da0b79c02062d7406982227c06630da2f9
Phoenix <phoenixdev1223@gmail.com>
2023-10-14 01:43:12 +0800

    update

--- scrape.js
+++ scrape.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const XLSX = require("xlsx");
-const FILE_NAME = '1-10000.xlsx';
+const FILE_NAME = '0-20000.xlsx';
 
 const scrape = async () => {
-    let index = 590
-    let end = 10000
+    let index = 0
+    let end = 20000
     let cnt = 0
 
-    while (index <= end) {
+    while (index < end) {
         await axios.get(`https://apps.calbar.ca.gov/attorney/Licensee/Detail/${index + 1}`).then((res) => {
             const $ = cheerio.load(res.data);
             if ($(`b:contains("#${index + 1}")`).text() !== '') {
@@ -20,51 +20,72 @@ const scrape = async () => {
                 const name = arr[0].trim();
                 const id = arr[1].trim();
 
-                var addressRaw = $('p:contains("Address:")').text().trim();
-                const address = addressRaw.split(":")[1].trim();
+                var addressRaw = $('p:contains("Address:")').text() !== '' ? $('p:contains("Address:")').text().trim() : '';
+                const address = addressRaw !== '' ? addressRaw.split(":")[1].trim() : '';
 
-                var phoneAndFax = $('p:contains("Phone:")').text().trim();
-                arr = phoneAndFax.split("|");
-                const phone = arr[0].split(":")[1].trim();
-                const fax = arr[1].split(":")[1].trim();
+                var phoneAndFax = $('p:contains("Phone:")').text() !== '' ? $('p:contains("Phone:")').text().trim() : '';
+                arr = $('p:contains("Phone:")').text() !== '' ? phoneAndFax.split("|") : '';
+                const phone = arr !== '' ? arr[0].split("Phone:")[1].trim() : '';
+                const fax = arr !== '' ? arr[1] ? arr[1].split("Fax:")[1].trim() : '' : '';
 
-                var emailAndWeb = $('p:contains("Email:")').text().trim();
-                arr = emailAndWeb.split("|");
-                const email = arr[0].split(":")[1].trim();
-                const website = arr[1].split(":")[1].trim();
+                var emailAndWeb = $('p:contains("Email:")').text() !== '' ? $('p:contains("Email:")').text().trim() : '';
+                arr = $('p:contains("Email:")').text() !== '' ? emailAndWeb.split("|") : '';
+                let email = ''
+                if (arr !== '') {
+                    let id = ''
+                    let index = res.data.indexOf(`{display:inline;}`)
+                    if (res.data[index - 3] === 'e') id = res.data.substring(index - 3, index)
+                    else id = res.data.substring(index - 2, index);
+                    email = $(`span#${id}`).text()
+                }
+                const website = arr !== '' ? arr[1] ? arr[1].split("Website:")[1].trim() : '' : '';
+
+                const ele = $('tbody tr:first-child')
+                const status = ele.find('span').text() ? ele.find('span').text().trim() : '';
+
+                const ele1 = $('tbody tr:last-child')
+                const admissionDate = ele1.find('strong').text() ? ele1.find('strong').text().trim() : '';
 
                 // A[*]: No, B[*]: Number, C[*]: Name, D[*]: Address, E[*]: Phone, F[*]: Fax, G[*]: Email, H[*]: Website
-                let cell = XLSX.utils.decode_cell(`A${cnt + 2}`);
+                let cell = XLSX.utils.decode_cell(`A${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`A${cnt + 2}`].v = cnt + 1;
+                worksheet[`A${cnt + 1}`].v = cnt + 1;
 
-                cell = XLSX.utils.decode_cell(`B${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`B${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`B${cnt + 2}`].v = name;
+                worksheet[`B${cnt + 1}`].v = name;
 
-                cell = XLSX.utils.decode_cell(`C${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`C${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`C${cnt + 2}`].v = id;
+                worksheet[`C${cnt + 1}`].v = id;
 
-                cell = XLSX.utils.decode_cell(`D${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`D${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`D${cnt + 2}`].v = address;
+                worksheet[`D${cnt + 1}`].v = address;
 
-                cell = XLSX.utils.decode_cell(`E${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`E${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`E${cnt + 2}`].v = phone;
+                worksheet[`E${cnt + 1}`].v = phone;
 
-                cell = XLSX.utils.decode_cell(`F${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`F${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`F${cnt + 2}`].v = fax;
+                worksheet[`F${cnt + 1}`].v = fax;
 
-                cell = XLSX.utils.decode_cell(`G${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`G${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`G${cnt + 2}`].v = email;
+                worksheet[`G${cnt + 1}`].v = email;
 
-                cell = XLSX.utils.decode_cell(`H${cnt + 2}`);
+                cell = XLSX.utils.decode_cell(`H${cnt + 1}`);
                 XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
-                worksheet[`H${cnt + 2}`].v = website;
+                worksheet[`H${cnt + 1}`].v = website;
+
+                cell = XLSX.utils.decode_cell(`I${cnt + 1}`);
+                XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
+                worksheet[`I${cnt + 1}`].v = status;
+
+                cell = XLSX.utils.decode_cell(`J${cnt + 1}`);
+                XLSX.utils.sheet_add_aoa(worksheet, [['']], { origin: cell });
+                worksheet[`J${cnt + 1}`].v = admissionDate;
 
                 XLSX.writeFile(workbook, FILE_NAME);
                 cnt++;
